adb1d4e55c96535c72b8c2dfdd4b4e6a283353b5
Amina Bruaset <aminabruaset@gmail.com>
2025-11-03 12:50:30 +0100

    Update main.py

--- innlogging/main.py
+++ innlogging/main.py
@@ -1,1 +1,31 @@
 # innloggingsprogram 
+
+def main():
+    state = "start"
+    while state != "quit":
+        if state == "start":
+            pass
+        elif state == "innLogget":
+            pass
+
+def vis_startmeny():
+    print("\n === Startmeny ===")
+    print("1)  Logg inn")
+    print("2)  Registrer ny bruker")
+    print("3)  Avslutt")
+    valg = input ("hva ønsker du å gjøre")
+    if valg == "1":
+        print("Du er nå logget inn.")
+        return "innLogget"
+    elif valg == "2":
+        print("Ny bruker registrert.")
+        return "start"
+    elif valg == "3":
+        return "quit"
+    else:
+        print("Ugyldig valg, prøv igjen.")
+        return "start"
+    while state != "quit":
+        if state == "start":
+            state = vis_startmeny()
+       
